06702c75af6ace45cab325ec274a6687df3f9631
crnbrdrck <broderickciaran@gmail.com>
2018-09-11 18:47:05 +0100

    Story tab switching TS done, gonna add some basic animation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, World!')
+function tabChange(e: Event) {
+  const tab = e.target as HTMLElement;
+  // Switch the active tab and story
+  const active = document.querySelector('.hero-foot .tabs li.is-active a')! as HTMLElement;
+  active.parentElement!.classList.remove('is-active');
+  document.getElementById(active.dataset.target!)!.classList.add('is-removed');
+  tab.parentElement!.classList.add('is-active');
+  document.getElementById(tab.dataset.target!)!.classList.remove('is-removed');
+}
+
+const tabs = document.querySelectorAll('.hero-foot .tabs a');
+for (let i = 0; i < tabs.length; i ++) {
+  tabs[i].addEventListener('click', tabChange, false);
+}
