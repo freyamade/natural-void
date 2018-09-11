8582c21a6a97ddaf79160f07191d5ce7b92664c1
Ciaran Broderick <broderickciaran@gmail.com>
2018-09-11 21:03:33 +0100

    Basic go webserver done

--- main.go
+++ main.go
@@ -2,10 +2,41 @@ package main
 
 import (
     "net/http"
+    "strings"
+    "html/template"
     "github.com/go-chi/chi"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./dist")))
-    http.ListenAndServe(":3000", nil)
+    r := chi.NewRouter()
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        tmpl, err := template.ParseFiles("templates/index.html")
+        if err != nil {
+            panic(err)
+        }
+        tmpl.Execute(w, nil)
+    })
+    // Serve the static files
+    FileServer(r, "/static", http.Dir("./static"))
+    http.ListenAndServe(":3333", r)
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+    if strings.ContainsAny(path, "{}*") {
+        panic("FileServer does not permit URL parameters.")
+    }
+
+    fs := http.StripPrefix(path, http.FileServer(root))
+
+    if path != "/" && path[len(path)-1] != '/' {
+        r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+        path += "/"
+    }
+    path += "*"
+
+    r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fs.ServeHTTP(w, r)
+    }))
 }
