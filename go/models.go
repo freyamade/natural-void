b30d7aa3aac6fee5b35a61b3a4f9ef49a76d3126
crnbrdrck <broderickciaran@gmail.com>
2018-09-12 12:42:28 +0100

    Turned basic structs into GORM models

--- go/models.go
+++ go/models.go
@@ -1,15 +1,21 @@
 package naturalvoid
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 // DB Models
 // Model representing a User in the DB
 // Users come from LDAP so this just stores some information for mapping stories to their DMs
 type User struct {
+	gorm.Model
 	ID int
 }
 
 // A story is a collection of episodes
 // ASTTW Chapter 1 is a story, as well as Delve Into Delirium
 type Story struct {
+	gorm.Model
 	Description []string
 	DM          User
 	ID          int
@@ -21,6 +27,7 @@ type Story struct {
 // An Episode relates to a recorded file.
 // Episodes are mapped to a Story for obvious reasons.
 type Episode struct {
+	gorm.Model
 	Description []string
 	Name        string
 	Number      int
