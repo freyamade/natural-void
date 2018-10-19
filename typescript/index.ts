6012fdfe6a50da1b3690f92f6c320ae8db870cae
Ciaran Broderick <cb@cix.ie>
2018-10-19 14:16:28 +0100

    Removing semicolons since I dont like them as much as I thought

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,19 +1,19 @@
 function tabChange(e: Event) {
-  const tab = e.target as HTMLElement;
+  const tab = e.target as HTMLElement
   // Switch the active tab and story
-  const active = document.querySelector('.hero-foot .tabs li.is-active a')! as HTMLElement;
-  active.parentElement!.classList.remove('is-active');
-  document.getElementById(active.dataset.target!)!.classList.add('is-removed');
-  tab.parentElement!.classList.add('is-active');
-  document.getElementById(tab.dataset.target!)!.classList.remove('is-removed');
+  const active = document.querySelector('.hero-foot .tabs li.is-active a')! as HTMLElement
+  active.parentElement!.classList.remove('is-active')
+  document.getElementById(active.dataset.target!)!.classList.add('is-removed')
+  tab.parentElement!.classList.add('is-active')
+  document.getElementById(tab.dataset.target!)!.classList.remove('is-removed')
 }
 
-const tabs = document.querySelectorAll('.hero-foot .tabs a');
-for (let i = 0; i < tabs.length; i ++) {
-  tabs[i].addEventListener('click', tabChange, false);
+const tabs = document.querySelectorAll('.hero-foot .tabs a')
+for (let i = 0 i < tabs.length; i ++) {
+  tabs[i].addEventListener('click', tabChange, false)
 }
 
 // Register service worker
 // if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/static/js/sw.js');
+//   navigator.serviceWorker.register('/static/js/sw.js')
 // }
--- typescript/listen.ts
+++ typescript/listen.ts
@@ -1,25 +1,25 @@
-import Amplitude from 'amplitudejs';
+import Amplitude from 'amplitudejs'
 
 interface Colours {
-  primary: string;
-  secondary: string;
+  primary: string
+  secondary: string
 }
 
 function getSchemeColours(): Colours {
   // The scheme colours can be retrieved from the hero and the border colour of the title
-  let primary, secondary: string;
-  primary = getComputedStyle(document.querySelector('.hero.is-scheme-primary')!).backgroundColor;
-  secondary = getComputedStyle(document.querySelector('.listen-title')!).borderBottomColor!;
-  return {primary: primary, secondary: secondary};
+  let primary, secondary: string
+  primary = getComputedStyle(document.querySelector('.hero.is-scheme-primary')!).backgroundColor
+  secondary = getComputedStyle(document.querySelector('.listen-title')!).borderBottomColor!
+  return {primary: primary, secondary: secondary}
 }
 
 async function loadEpisode(): Promise<void> {
   // Get the episode id
-  const dataset = (document.querySelector('#player-container')! as HTMLElement).dataset;
-  const episodeNum = dataset.episodeNum!;
-  const storyId = dataset.storyId;
+  const dataset = (document.querySelector('#player-container')! as HTMLElement).dataset
+  const episodeNum = dataset.episodeNum!
+  const storyId = dataset.storyId
   // Get the colours of the scheme
-  const colours: Colours = getSchemeColours();
+  const colours: Colours = getSchemeColours()
   Amplitude.init({
     bindings: {
       32: 'play_pause',
@@ -27,15 +27,15 @@ async function loadEpisode(): Promise<void> {
     songs: [{
       url: `/episodes/${storyId}/${episodeNum}`,
     }]
-  });
+  })
 
   // Allow for clicking to move around on the progress bar
   document.getElementById('song-played-progress')!.addEventListener('click', (e: MouseEvent) => {
-    const elmnt = e.target as HTMLElement;
-    const offset = elmnt.getBoundingClientRect();
-    const x = e.pageX - offset.left;
-    Amplitude.setSongPlayedPercentage((x / elmnt.offsetWidth) * 100);
-  });
+    const elmnt = e.target as HTMLElement
+    const offset = elmnt.getBoundingClientRect()
+    const x = e.pageX - offset.left
+    Amplitude.setSongPlayedPercentage((x / elmnt.offsetWidth) * 100)
+  })
 }
 
-loadEpisode();
+loadEpisode()
